5ee8859ae1e7328d71d43a59308bfffc71ea2eb0
technodog <pd@technodog.com>
2024-10-30 21:11:37 -0700

    Update staff.php
    cleared out

--- staff.php
+++ staff.php
@@ -21,10 +21,6 @@
 
         <ul>
             <li>Patrick Dent</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
-            <li>Studet 5</li>
-            <li>Studet 6</li>
             <!-- add more -->
         </ul>
 
